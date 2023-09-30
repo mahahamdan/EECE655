3848791f285f44ad793a508feb17a6317b52f5b1
Bechara Rizk <becharaerizk@yahoo.com>
2023-09-30 09:06:48 +0300

    first version of the detection algorithm but a lot of false positives so need to see what's wrong

--- detectAttack/main.py
+++ detectAttack/main.py
@@ -1,5 +1,7 @@
 import threading
-from scapy.all import IPv6, IP, TCP, Ether, sniff
+# pip install thread6
+from scapy.all import IPv6, IP, TCP, Ether, sniff, get_if_addr, conf, get_if_hwaddr
+# pip install scapy
 
 # we will use threading to run multiple functions at the same time 
 # and thus not lose any packet while processing the data
@@ -62,14 +64,20 @@ def packet_extract(packet):
 def detectAttack(sharedData):
     # i will use an index to keep track of where i am in the list in case the list gets updated while i'm processing data
     index=0
+    # i will use a local variable to store the data that i will process
     local=[]
+    # i will use a dictionary to store the statistics of the connections
+    stats={}
+    # i will use a variable to store my IP address so that i can know if the connection is incoming or outgoing
+    myIp=get_if_addr(conf.iface)
+    myMac=get_if_hwaddr(conf.iface)
     while True:
         if len(sharedData) > index:
             # in this if condition i check if there is new data in the list for me to use 
 
             #if there is i will take it and save it to variable local to my thread
             local=sharedData[index]
-            # we could then in order to save storage set the data in the list to None so that we know that this data was already processed
+            # to save storage we could set the data in the list to None so that we know that this data was already processed
             #sharedData[index] = None
 
             # we increment the index so that we can check when new data is added to the list
@@ -78,15 +86,66 @@ def detectAttack(sharedData):
         if local != []:
             # here i will process the data using the the local variable
 
+            # i will check if the connection is incoming or outgoing
+            
+            if local['srcIp']!=myIp and local['dstIp']==myIp:
+                # the connection is incoming
+                # identify the connection by the source IP and port
+                identifier=f"{local['srcIp']}:{local['srcPort']}"
+                if identifier not in stats:
+                    # if the connection is new i will add it to the dictionary
+                    stats[identifier]={'Ack':local['ackNb'],
+                                       'Seq':local['seqNb'],
+                                       'flags':str(local['flags']),
+                                       'srcMac':local['srcMac'],
+                                       'time':int(local['time']),
+                                       'srcPort':local['srcPort'],
+                                       'srcIp':local['srcIp'],}
+                else:
+                    newAck=local['ackNb']
+                    newSeq=local['seqNb']
+                    newFlags=str(local['flags'])
+                    newSrcMac=local['srcMac']
+                    newTime=local['time']
+                    newMac=local['srcMac']
+
+                    if newSrcMac!=stats[identifier]['srcMac']:
+                        print("=============== Need to check ===============")
+
+                    if 'R' in stats[identifier]['flags']:
+                        # if the connection was reset previously, 
+                        # check if someone is trying to open a new connection with syn flag
+                        # of if someone is trying to continue the connection (which is when the attack happened)
+                        if 'S' in newFlags:
+                            stats[identifier]['flags']=newFlags
+                            stats[identifier]['time']=newTime
+                            stats[identifier]['srcMac']=newSrcMac
+                            stats[identifier]['Ack']=newAck
+                            stats[identifier]['Seq']=newSeq
+                        else:
+                            # if the connection was reset and someone is trying to continue it
+                            # then an attack happened in the packet that sent the RST flag
+                            print("=============== Attack detected ===============\nDetails of the connection that sent the RST flag:")
+                            print(f"IP: {stats[identifier]['srcIp']}\nPort: {stats[identifier]['srcPort']}\nMAC: {stats[identifier]['srcMac']}\nTime: {stats[identifier]['time']}\nFlags: {stats[identifier]['flags']}")
+                            print("=============== =============== ===============\nDetails of the connection that tried to continue:")
+                            print(f"IP: {local['srcIp']}\nPort: {local['srcPort']}\nMAC: {newMac}\nTime: {int(newTime)}\nFlags: {newFlags}")
+                    else:
+                        stats[identifier]['flags']=newFlags
+                        stats[identifier]['time']=int(newTime)
+                        stats[identifier]['srcMac']=newSrcMac
+                        stats[identifier]['Ack']=newAck
+                        stats[identifier]['Seq']=newSeq
+            
 
             # after i'm done processing the data i will set the local variable to an empty list
             # so that i don't process the same data again
             local=[]
+            # print(stats)
 
-            
 
 
 # this list will be used so the two threads can share data between them
+# it is basically a queue shared between threads running concurrently
 sharedData = []
 
 # creating the threads
